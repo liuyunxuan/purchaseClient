fa70790855415c1971edb8f88933b11709ac4419
本机 <liuyunxuan@liuyunxuandemacbook-air.local>
2016-01-14 16:44:33 +0800

    Initial Commit

---
+++ PurchaseClient/AppDelegate/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  PurchaseClient
+//
+//  Created by  liuyunxuan on 16/1/14.
+//  Copyright © 2016年  liuyunxuan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ PurchaseClient/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  PurchaseClient
+//
+//  Created by  liuyunxuan on 16/1/14.
+//  Copyright © 2016年  liuyunxuan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
